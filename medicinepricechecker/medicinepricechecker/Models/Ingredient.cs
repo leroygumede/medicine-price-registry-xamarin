da79752b5ffd4c855558a8426714ca33f2813065
LeRoy Gumede <leroygumede@gmail.com>
2017-08-27 12:20:15 +0200

    Added Models
    - Add products and ingredient models

---
+++ medicinepricechecker/medicinepricechecker/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace medicinepricechecker.Models
+{
+    public class Ingredient : INotifyPropertyChanged
+    {
+        public double strength { get; set; }
+        public string unit { get; set; }
+        public string name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ medicinepricechecker/medicinepricechecker/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace medicinepricechecker.Models
+{
+    public class Products : INotifyPropertyChanged
+    {
+        public string name { get; set; }
+        public string nappi_code { get; set; }
+        public string schedule { get; set; }
+        public string cost_per_unit { get; set; }
+        public int num_packs { get; set; }
+        public string dispensing_fee { get; set; }
+        public IList<Ingredient> ingredients { get; set; }
+        public int pack_size { get; set; }
+        public string dosage_form { get; set; }
+        public string is_generic { get; set; }
+        public string regno { get; set; }
+        public int id { get; set; }
+        public string sep { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
