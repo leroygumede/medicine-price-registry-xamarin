94cee0897d7259a67928866b80ec20d182d88de4
LeRoy Gumede <leroygumede@gmail.com>
2017-08-27 10:42:54 +0200

    Project init
    - Added project files
    - updated gitignore.md

---
+++ medicinepricechecker/medicinepricechecker/medicinepricecheckerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace medicinepricechecker
+{
+    public partial class medicinepricecheckerPage : ContentPage
+    {
+        public medicinepricecheckerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
