909d54363c523d36dd8ba4310eca331fd08e8a0e
LeRoy Gumede <leroygumede@gmail.com>
2017-08-27 11:30:03 +0200

    Added pages
    - Added about page
    -Added details page

---
+++ medicinepricechecker/medicinepricechecker/PageModels/DetailsPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FreshMvvm;
+
+namespace medicinepricechecker
+{
+    public class DetailsPageModel : FreshBasePageModel
+    {
+        public DetailsPageModel()
+        {
+        }
+    }
+}
